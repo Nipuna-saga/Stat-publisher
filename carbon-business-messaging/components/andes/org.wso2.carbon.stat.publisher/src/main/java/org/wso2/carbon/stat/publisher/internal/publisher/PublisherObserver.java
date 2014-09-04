3c6b0c52a3b51aa079465614b2b1ceecbdcc8245
Nipuna-saga <nipuna@wso2.com>
2014-09-04 17:25:09 +0530

    merge

--- carbon-business-messaging/components/andes/org.wso2.carbon.stat.publisher/src/main/java/org/wso2/carbon/stat/publisher/internal/publisher/PublisherObserver.java
+++ carbon-business-messaging/components/andes/org.wso2.carbon.stat.publisher/src/main/java/org/wso2/carbon/stat/publisher/internal/publisher/PublisherObserver.java
@@ -17,7 +17,7 @@ import java.util.TimerTask;
  */
 public class PublisherObserver {
 
-    public static Timer timer ;
+
     StatConfigurationDTO statConfigurationDTOObject;
     StatConfiguration statConfigurationInstance;
     DataAgent dataAgentInstance;
@@ -36,7 +36,7 @@ public class PublisherObserver {
 
     //Timer task to publish system and MB stats
     public void statPublisherTimerTask() {
-
+        Timer timer;
 
         TimerTask taskPublishStat = new TimerTask() {
 
@@ -44,72 +44,56 @@ public class PublisherObserver {
             @Override
 
             public void run() {
+
                 try {
                     PrivilegedCarbonContext.startTenantFlow();
-                    PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantId(tenantID,true);
-
-<<<<<<< HEAD
+                    PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantId(tenantID, true);
 
 
-                statConfigurationInstance = statConfigurationDTOObject.ReadRegistry(tenantID); //get statConfiguration Instance according to tenant ID
-
-                    System.out.println("timer task activated" + tenantID);
-=======
-                //    statConfigurationDTOObject = new StatConfigurationDTO();
-
-                statConfigurationInstance = statConfigurationDTOObject.ReadRegistry(tenantID); //get statConfiguration Instance according to tenant ID
+                    statConfigurationInstance = statConfigurationDTOObject.ReadRegistry(tenantID); //get statConfiguration Instance according to tenant ID
 
 
-                //if (statConfigurationInstance.isEnableStatPublisher()) { //check Stat publisher Enable
+                    //if (statConfigurationInstance.isEnableStatPublisher()) { //check Stat publisher Enable
 
                     dataAgentInstance = DataAgent.getObjectDataAgent();
-                //todo uncomment this line
+                    //todo uncomment this line
 //String URL = statConfigurationInstance.getURL();
->>>>>>> 26a9d8ce475da9ac4763a33d4c4965ad6246c012
 
 //todo remove this line
-                String URLList = "tcp://localhost:7611";
+                    String URLList = "tcp://localhost:7611";
 
-                URLOperations urlOperations = new URLOperations();
-                String URLArray[] = urlOperations.URLSplitter(URLList);
-                String[] credentials={"admin","admin"};
+                    URLOperations urlOperations = new URLOperations();
+                    String URLArray[] = urlOperations.URLSplitter(URLList);
+                    String[] credentials = {"admin", "admin"};
 
-<<<<<<< HEAD
-                //    dataAgentInstance = DataAgent.getObjectDataAgent();
-=======
-                for(String URL : URLArray) {
->>>>>>> 26a9d8ce475da9ac4763a33d4c4965ad6246c012
+                    for (String URL : URLArray) {
 
-                    //if (statConfigurationInstance.isSystem_statEnable()) {//check system stat enable configuration
+                        //if (statConfigurationInstance.isSystem_statEnable()) {//check system stat enable configuration
 
-                    //System.out.println("System stat Publishing activated" + tenantID);
+                        //System.out.println("System stat Publishing activated" + tenantID);
 
-                    System.out.println(URL);
-                    dataAgentInstance.sendSystemStats(URL,credentials);
+                        System.out.println(URL);
+                        dataAgentInstance.sendSystemStats(URL, credentials);
+
+                        //}
+                        //if (statConfigurationInstance.isMB_statEnable()) {//check MB stat enable configuration
+
+                        // System.out.println("MB stat Publishing activated" + tenantID);
+
+                        //   dataAgentInstance.sendMBStatistics(statConfigurationInstance);
+                        // }
+                    }
+
 
                     //}
-                    //if (statConfigurationInstance.isMB_statEnable()) {//check MB stat enable configuration
-
-                   // System.out.println("MB stat Publishing activated" + tenantID);
-
-                    //   dataAgentInstance.sendMBStatistics(statConfigurationInstance);
-                    // }
-                }
-
-
-<<<<<<< HEAD
-                }
                 } catch (Exception e) {
                     //  log.error("failed to update statics from BAM publisher", e);
                 } finally {
                     PrivilegedCarbonContext.endTenantFlow();
-                }   }
-
-
-=======
-                //}
+                }
             }
->>>>>>> 26a9d8ce475da9ac4763a33d4c4965ad6246c012
+
+
         };
 
         timer = new Timer();
@@ -121,37 +105,21 @@ public class PublisherObserver {
     //method to publish message statistics
     public void messageStatPublisherTask(AndesMessageMetadata message) {
 
-<<<<<<< HEAD
-
-       if (statConfigurationInstance.isEnableStatPublisher()) { //check Stat publisher Enable
-
-           if (statConfigurationInstance.isMessage_statEnable()) { //check message stat enable configuration
-=======
 //todo enable if
-       // if (statConfigurationInstance.isEnableStatPublisher()) { //check Stat publisher Enable
+        // if (statConfigurationInstance.isEnableStatPublisher()) { //check Stat publisher Enable
 //todo enable if
-           // if (statConfigurationInstance.isMessage_statEnable()) { //check message stat enable configuration
->>>>>>> 26a9d8ce475da9ac4763a33d4c4965ad6246c012
+        // if (statConfigurationInstance.isMessage_statEnable()) { //check message stat enable configuration
 
-                System.out.println("Message stat Publishing activated" + tenantID + message.getDestination());
+        System.out.println("Message stat Publishing activated" + tenantID + message.getDestination());
 
-                //   dataAgentInstance=DataAgent.getObjectDataAgent();
-                //   dataAgentInstance.sendMessageStatistics(statConfigurationInstance,message);
-<<<<<<< HEAD
-                PublisherObserver publisherObserverInstance = new PublisherObserver();
-=======
+        //   dataAgentInstance=DataAgent.getObjectDataAgent();
+        //   dataAgentInstance.sendMessageStatistics(statConfigurationInstance,message);
         //todo move this to activator method
-               PublisherObserver publisherObserverInstance = new PublisherObserver();
->>>>>>> 26a9d8ce475da9ac4763a33d4c4965ad6246c012
-                publisherObserverInstance.statPublisherTimerTask();
+        PublisherObserver publisherObserverInstance = new PublisherObserver();
+        publisherObserverInstance.statPublisherTimerTask();
 
 
-<<<<<<< HEAD
-       }
-=======
->>>>>>> 26a9d8ce475da9ac4763a33d4c4965ad6246c012
-
-           // }
+        // }
 
 //        }
 
@@ -171,11 +139,6 @@ public class PublisherObserver {
 
 
             }
-<<<<<<< HEAD
-        }
-    }
-=======
->>>>>>> 26a9d8ce475da9ac4763a33d4c4965ad6246c012
 
         }
     }
