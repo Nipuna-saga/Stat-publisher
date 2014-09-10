ef7f6b985a6154d8b5f6646868a21cb464621b6d
dilshani@wso2.com <dilshani@wso2.com>
2014-09-10 09:56:40 +0530

    patch 05

--- carbon-business-messaging/components/andes/org.wso2.carbon.stat.publisher/src/main/java/org/wso2/carbon/stat/publisher/internal/conf/ConfConstants.java
+++ carbon-business-messaging/components/andes/org.wso2.carbon.stat.publisher/src/main/java/org/wso2/carbon/stat/publisher/internal/conf/ConfConstants.java
@@ -5,5 +5,5 @@ package org.wso2.carbon.stat.publisher.internal.conf;
  */
 public class ConfConstants {
     public static final String JMXFilePath = "repository/conf/etc/jmx.xml";
-    public static final String ConfDataFilePath = "/home/dilshani/OnGoing/My fork/Stat-publisher-1/carbon-business-messaging/components/andes/org.wso2.carbon.stat.publisher/src/main/java/org/wso2/carbon/stat/publisher/internal/conf/ConfigurationData.xml";
+    public static final String ConfDataFilePath = "repository/conf/ConfigurationData.xml";
 }
